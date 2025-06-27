2e435b0cec42cc6f732a21a703dbf86cc22acabc
Shreemathi <shreemathi023@gmail.com>
2025-06-27 11:10:04 +0530

    Initial commit of Thalir Tech website

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Thalir Tech site loaded.");
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-74CRG7L496"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-74CRG7L496');
+</script>
